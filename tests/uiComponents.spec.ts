1a0063c01f528fff168f53747572782b712faeea
n3xtb1te <168577163+n3xtb1te@users.noreply.github.com>
2024-09-06 13:29:00 +0300

    add tooltips

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -89,3 +89,15 @@ test.skip('lists and dropdowns', async ({ page }) => {
         }
     }
 });
+
+test('tooltips', async ({ page }) => {
+    await page.getByText('Modal & Overlays').click();
+    await page.getByText('Tooltip').click();
+
+    const tooltipCard = page.locator('nb-card', { hasText: 'Tooltip Placements' });
+    await tooltipCard.getByRole('button', { name: 'Top' }).hover();
+
+    page.getByRole('tooltip'); // if you have a role tooltip created
+    const tooltip = await page.locator('nb-tooltip').textContent();
+    expect(tooltip).toEqual('This is a tooltip');
+});
